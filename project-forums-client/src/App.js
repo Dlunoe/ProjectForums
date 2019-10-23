e7f639891ac8092ecabed61b6ffe5b9e70e455d6
Dustin Lunoe <dlunoe@yahoo.com>
2019-10-23 17:09:51 -0600

    refactored App to class component

--- project-forums-client/src/App.js
+++ project-forums-client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Navigation from '../src/Components/NavigationComponent/Navigation';
@@ -8,18 +8,39 @@ import SignInPage from './Components/User/UserLoginComponent/Login'
 import * as ROUTES from './Constants/Routes';
 import { SignUpForm } from './Components/User/UserRegisterComponent/UserRegister';
 
-const App = () => (
-  <Router>
-    <div>
-      <Navigation/>
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-      <hr />
+    this.state = {
+      authUser: null,
+    }
+  }
 
-      <Route path={ROUTES.FORUMS} component={ForumsPage}/>
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignUpForm} />
-    </div>
-  </Router>
-)
+  render() {
+    return(
+      <Router>
+        <div>
+          <Navigation/>
+
+          <hr />
+
+          <Route path={ROUTES.FORUMS} component={ForumsPage}/>
+          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+          <Route path={ROUTES.SIGN_UP} component={SignUpForm} />
+        </div>
+      </Router>
+    )
+  }
+}
+
+
+
+
+
+
+
+  
+
 
 export default App;
