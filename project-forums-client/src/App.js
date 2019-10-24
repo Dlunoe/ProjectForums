74a5ddc0dda929c1763febe5cd99b646c4156d5a
Dustin Lunoe <dlunoe@yahoo.com>
2019-10-24 12:03:55 -0600

    App is back to a functional component, authentication runs through an independant component w/ context

--- project-forums-client/src/App.js
+++ project-forums-client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Navigation from '../src/Components/NavigationComponent/Navigation';
@@ -7,49 +7,21 @@ import SignInPage from './Components/User/UserLoginComponent/Login'
 
 import * as ROUTES from './Constants/Routes';
 import { SignUpForm } from './Components/User/UserRegisterComponent/UserRegister';
-import { withFirebase } from '../src/Components/Firebase';
-import { AuthUserContext } from '../src/Components/Sessions';
+import { withAuthentication } from './Components/Sessions';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => (
+  <Router>
+    <div>
+    <Navigation />
 
-    this.state = {
-      authUser: null,
-    }
-  }
+    <hr />
 
-  componentDidMount() {
-    this.listener = this.props.firebase.auth.onAuthStateChanged(
-      authUser =>{
-        authUser
-          ? this.setState({ authUser })
-          : this.setState({ authUser: null })
-    })
-  }
+    <Route path={ROUTES.FORUMS} component={ForumsPage}/>
+    <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+    < Route path={ROUTES.SIGN_UP} component={SignUpForm} />
+    </div>
+  </Router>
+)
 
-  componentWillUnmount(){
-    this.listener();
-  }
 
-  render() {
-    return(
-      <AuthUserContext.Provider value={this.state.authUser}>
-        <Router>
-          <div>
-            <Navigation />
-
-            <hr />
-
-            <Route path={ROUTES.FORUMS} component={ForumsPage}/>
-            <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-          < Route path={ROUTES.SIGN_UP} component={SignUpForm} />
-          </div>
-        </Router>
-      </AuthUserContext.Provider>
-      
-    )
-  }
-}
-
-export default withFirebase(App);
+export default withAuthentication(App);
